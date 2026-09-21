08ad2414b0ba138d9f81fda8e6632a120fb1fadf
Jeremy Kenedy <jeremykenedy@gmail.com>
2026-09-21 01:14:06 -0700

    Align empty class formatting and avoid duplicate pull request builds

--- src/App/Http/Controllers/Controller.php
+++ src/App/Http/Controllers/Controller.php
@@ -3,7 +3,11 @@
 namespace jeremykenedy\LaravelPhpInfo\App\Http\Controllers;
 
 if (class_exists('App\Http\Controllers\Controller') && method_exists('App\Http\Controllers\Controller', 'middleware')) {
-    abstract class Controller extends \App\Http\Controllers\Controller {}
+    abstract class Controller extends \App\Http\Controllers\Controller
+    {
+    }
 } else {
-    abstract class Controller extends \Illuminate\Routing\Controller {}
+    abstract class Controller extends \Illuminate\Routing\Controller
+    {
+    }
 }
